28a6ad05e6f4f04177ae3b8436853548171113aa
vmusser <vmusser@gmail.com>
2020-02-08 16:37:31 -0600

    Added motors in B C and D

--- include/motors.h
+++ include/motors.h
@@ -27,26 +27,24 @@ MotorGroup intake({intakeLeft,intakeRight});
 Motor tilter(3, true, AbstractMotor::gearset::red);
 auto tilterEncoder = IntegratedEncoder(tilter);
 
-pros::ADIAnalogIn lineTracker ('B');
+
+pros::ADIDigitalIn tilterSensor ('B');
+pros::ADIAnalogIn lineSensor ('C');
+pros::ADIDigitalIn dr4bSensor ('D'); 
 //lineTracker.get_value() (0 --> 4095): 0 being light and 4095 being dark
 
 /*----------TEMPLATES------------------
 okapi::MotorGroup right2({10, 20});
-
 auto rightEB2 = IntegratedEncoder(11);
-
 auto chassis2 = ChassisControllerFactory::create(
     left2, right2,
     AbstractMotor::gearset::green,
     {wheel diameter, 1.0});
 };
-
 Motor dr4bL2 = 15;
-
 auto dr4b2 = ChassisControllerFactory::create(
     dr4bL2, dr4bR2,
     AbstractMotor::gearset::red
     );
-
 ADIGyro gyro = ADIGyro('A', .1);
 */
