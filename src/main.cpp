2cf4d80de0b7276e277e41271cdc153d2f6b5753
Aditya Narayanan <aditya.narayanan03@gmail.com>
2020-02-18 15:39:28 -0600

    Added big side auton (untested)

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@ void autonomous() {
     /* 6pt pseudo auton on aditya's newest code*/
 
 
-/*
+/* Red Small Side
     forward(10.0, 100);
     backward(10.0, 100);
     while(tilterEncoder.get() < 700.0)
@@ -52,6 +52,41 @@ void autonomous() {
     backward(10.0, 80);
 */
 
+    // /*Red Big Side
+    forward(10.0, 100);
+    backward(10.0, 100);
+    while(tilterEncoder.get() < 700.0)
+      {tilter.moveVelocity(100);}
+    intake.moveVelocity(100);
+    pros::delay(200);
+    intake.moveVelocity(0);
+    dr4bDownMacro();
+
+    intake.moveVelocity(-100);
+    forward(24, 60);
+    intake.moveVelocity(0);
+    rightTurn(90, 80, 1.0);
+
+    intake.moveVelocity(-100);
+    forward(18.0, 60);
+    intake.moveVelocity(0);
+    
+    backward(10.0, 80);
+    rightTurn(170, 80, 1.0);
+
+    intake.moveVelocity(-100);
+    forward(26.0, 60);
+    intake.moveVelocity(0);
+
+    leftTurn(50, 80, 1.0);
+    forward(13.5, 120);
+    intake.moveVelocity(100);
+    pros::delay(300);
+    intake.moveVelocity(0);
+    tilterMacro();
+    backward(10.0, 80);
+    // */
+
     //SKILLS AUTON
     /*
     forward(10.0, 100);
@@ -62,7 +97,7 @@ void autonomous() {
     pros::delay(200);
     intake.moveVelocity(0);
     dr4bDownMacro();
-    */
+    
 
     intake.moveVelocity(-100);
     forward(38, 60);
@@ -80,7 +115,7 @@ void autonomous() {
     intake.moveVelocity(0);
     tilterMacro();
     backward(10.0, 80);
-
+`   */
 
 } //Maybe make one of those...
 
@@ -97,11 +132,11 @@ void opcontrol()
     dr4b.setBrakeMode(AbstractMotor::brakeMode::hold);
 
     Controller masterController; //Maybe this can go in motor.h?
-
     pros::Task my_task(macroTask, (void *)"PROS", TASK_PRIORITY_DEFAULT, //start running the macro task. For this build, it WILL get stuck in macro1();. However, operator control should still work
                        TASK_STACK_DEPTH_DEFAULT, "My Task");
 
     while(true){
+        masterController.setText(0,0,std::to_string(tilter.getTemperature()));
         isZero = intakeControl(masterController.getDigital(ControllerDigital::R2), masterController.getDigital(ControllerDigital::R1), isZero); //Intake is currently in a task alone
 
         chassis.arcade(masterController.getAnalog(ControllerAnalog::leftX),
